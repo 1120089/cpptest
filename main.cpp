ac49b428da081ff0f89c7b165af0b78666a5f4da
1120089 <s1120089@dmhs.kh.edu.tw>
2025-09-22 03:20:11 +0000

    第一次提交

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
  #include <iostream>
- int main() {
-  int year = 2025;                 // 整數（沒有小數點）
-  double price = 10.99;            // 浮點數（有小數點）
-  double gpa = 2.5, temperature = 25.1; // 同行可宣告多個
-   std::cout << "year=" << year << "\n";
- std::cout << "price=" << price << "\n";
- std::cout << "gpa=" << gpa << ", temp=" << temperature << "\n";
-  return 0;
- }
+using namespace std;
+
+int main() {
+    float fahrenheit, celsius;
+
+    // 輸入華氏溫度
+    cout << "請輸入華氏溫度: ";
+    cin >> fahrenheit;
+
+    // 計算攝氏溫度
+    celsius = (fahrenheit - 32) * 5 / 9;
+
+    // 輸出結果
+    cout << "攝氏溫度是: " << celsius << "°C" << endl;
+
+    return 0;
+}
     
