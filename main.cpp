81c9258aa899e942c6af276505b3b29017eb2854
1120089 <s1120089@dmhs.kh.edu.tw>
2025-10-20 10:45:42 +0800

    hw2

--- main.cpp
+++ main.cpp
@@ -1,19 +1,46 @@
- #include <iostream>
+
+
+#include <iostream>
 using namespace std;
 
+void calculate_average() {
+    float scores[3];
+    float average = 0;
+    
+    
+    for (int i = 0; i < 3; ++i) {
+        cout << "請輸入第" << i + 1 << "次分數: ";
+        cin >> scores[i];
+    }
+
+    
+    for (int i = 0; i < 3; ++i) {
+        average += scores[i];
+    }
+    average /= 3;
+
+    
+    cout << "平均分數: " << average << endl;
+
+    
+    bool is_fail = false;
+    for (int i = 0; i < 3; ++i) {
+        if (scores[i] < 40) {
+            is_fail = true;
+            break;
+        }
+    }
+
+    if (is_fail || average < 60) {
+        cout << "此學科不及格" << endl;
+    } else {
+        cout << "此學科及格" << endl;
+    }
+}
+
 int main() {
-    float fahrenheit, celsius;
-
-    // 輸入華氏溫度
-    cout << "請輸入華氏溫度: ";
-    cin >> fahrenheit;
-
-    // 計算攝氏溫度
-    celsius = (fahrenheit - 32) * 5 / 9;
-
-    // 輸出結果
-    cout << "攝氏溫度是: " << celsius << "°C" << endl;
-
+    calculate_average();
     return 0;
 }
-    
+
+
