f6db93f008b93fbe8506ad8ba834e7fd728e5122
1120089 <s1120089@dmhs.kh.edu.tw>
2025-11-03 10:21:30 +0800

    hw4

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
-#include <iostream>  
+#include <iostream>
 using namespace std;
 
 int main() {
-    int sum = 0; 
+    int i = 1; // 外層控制「被乘數」
 
-    for (int i = 1; i <= 10; i++) { 
-        sum += i; 
-
-        if (i % 2 == 0) { 
-            cout << "偶數：" << i << endl;
+    while (i <= 9) {
+        int j = 1; // 內層控制「乘數」
+        while (j <= 9) {
+            cout << i << " x " << j << " = " << i * j << "\t";
+            j++;
         }
+        cout << endl; // 換行
+        i++;
     }
-
-    cout << "1 到 10 的總和是：" << sum << endl;
 
     return 0;
 }
