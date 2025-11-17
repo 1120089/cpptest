1ec0af841b9834281d98db31112e44fca2a8f0d7
1120089 <s1120089@dmhs.kh.edu.tw>
2025-11-17 10:56:21 +0800

    hw4
    cpp

--- hw4.cpp
+++ hw4.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
 using namespace std;
-void adding();          //沒有傳回值及參數, 需宣告為 void 型別 
 
-int main()
-{
-  for (int i = 0; i < 3; i++) // 呼叫 adding() 三次 
-    adding();
+// BMI 函式：傳回計算好的 BMI 值
+double calcBMI(double weight, double height) {
+    // height 以公尺計算
+    return weight / (height * height);
 }
 
-void adding(void)
-{
-  int num = 100;       // 局部變數 
-  cout << "num = " << num++ << endl;
+int main() {
+    double weight, height;
+
+    cout << "請輸入體重(kg): ";
+    cin >> weight;
+
+    cout << "請輸入身高(m): ";
+    cin >> height;
+
+    double bmi = calcBMI(weight, height);
+    cout << "您的 BMI 為: " << bmi << endl;
+
+    // 可選：BMI 分類判斷
+    if (bmi < 18.5)
+        cout << "體重過輕\n";
+    else if (bmi < 24)
+        cout << "正常範圍\n";
+    else if (bmi < 27)
+        cout << "過重\n";
+    else
+        cout << "肥胖\n";
+
+    return 0;
 }
