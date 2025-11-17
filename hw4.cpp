8ce91237035e24c1c4155b5dc76b9b8d177a5f7c
1120089 <s1120089@dmhs.kh.edu.tw>
2025-11-17 10:23:19 +0800

    hw4

--- hw4.cpp
+++ hw4.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
 using namespace std;
+void adding();          //沒有傳回值及參數, 需宣告為 void 型別 
 
-int main() {
-    int i = 1; // 外層控制「被乘數」
+int main()
+{
+  for (int i = 0; i < 3; i++) // 呼叫 adding() 三次 
+    adding();
+}
 
-    while (i <= 9) {
-        int j = 1; // 內層控制「乘數」
-        while (j <= 9) {
-            cout << i << " x " << j << " = " << i * j << "\t";
-            j++;
-        }
-        cout << endl; // 換行
-        i++;
-    }
-
-    return 0;
+void adding(void)
+{
+  int num = 100;       // 局部變數 
+  cout << "num = " << num++ << endl;
 }
